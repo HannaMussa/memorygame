425004f9ebe73eb1288ed63c71e3825a8e0d7b99
HannaMussa <hannamussaa@gmail.com>
2025-06-01 00:54:30 +0100

    Add event listener for document loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// When the page loads, initialize the game 
+window.addEventListener("DOMContentLoaded", function() {
+  resetGame();                          // Setup and shuffle cards, start timer
+  restartButton.addEventListener("click", resetGame); 
+});
+
+
 function flipCard(card) {
 }
 
